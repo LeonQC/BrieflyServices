2144de44eeb445f3fc51fe230b5d06000c127c73
kejin geng <kejingeng@kejins-MacBook-Pro.local>
2021-11-05 19:05:51 -0700

    Upload backend files

---
+++ src/test/java/com/briefly/projectbrefly/ProjectBreflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briefly.projectbrefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBreflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
